c3acbe7145897fa95f0a4f0f53ad7c1a9fd9990b
Prithivi36 <prithivipappian@gmail.com>
2025-06-10 09:35:50 +0530

    Lazy init && Inner Beans

--- src/main/java/com/learn/Desktop.java
+++ src/main/java/com/learn/Desktop.java
@@ -2,6 +2,9 @@ package com.learn;
 
 public class Desktop implements Computer {
 
+    public Desktop() {
+        System.out.println("Desktop created");
+    }
     @Override
     public void compile() {
         System.out.println("Compiling using Desktop...");
